0c6df34e069c80bcb4b747b7c44557514f7f9d00
Daniel Molnar <daniel4molnar@gmail.com>
2021-09-30 00:42:05 +0200

    add asset prefix, base path & imageUrl

--- components/cards/PresentationCard.tsx
+++ components/cards/PresentationCard.tsx
@@ -7,12 +7,14 @@ interface PresentationCardProps {
     icon: string;
 }
 
+const prefix = process.env.NEXT_PUBLIC_BASE_PATH || '';
+
 const PresentationCard: FunctionComponent<PresentationCardProps> = ({ title, text, icon }) => {
 
     return (
         <div className={styles.card}>
             <div className={styles.card_Icon}>
-                <img src={icon} alt="Icon" />
+                <img src={`${prefix}/${icon}`} alt="Icon" />
             </div>
             <p className={styles.card_Title}>{title}</p>
             <span className={styles.card_Text}>{text}</span>
